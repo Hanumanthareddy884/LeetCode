420ab41a4ba205fbc7caf6d5bcd3c18145e57d27
Hanumanthareddy H <109300961+Hanumanthareddy884@users.noreply.github.com>
2025-07-13 22:59:06 +0530

    Time: 11 ms (15.11%), Space: 17.9 MB (40.1%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "CD": 400,
+            "CM": 900,
+            "XC": 90,
+            "XL": 40,
+            "IX": 9,
+            "IV": 4,
+        }
+        k=0
+        c=0
+        for i in range(0,len(s)):
+            if dic.keys().__contains__(s[k:k+2]):
+                c+=dic[s[k:k+2]]
+                k+=2
+            elif len(s)<=k:
+                break
+            else :
+                c+=dic[s[k]]
+                k+=1
+        print(c)
+        return c
